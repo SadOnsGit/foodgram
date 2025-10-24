e8fdc246f608252d020eda2fdc81c73bc3f435bd
SadOnsGit <aid.torres09@yandex.ru>
2025-10-24 16:04:37 +0300

    add .env file

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -1,15 +1,19 @@
+import os
 from datetime import timedelta
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = (
-    "django-insecure-&gm*3e9h*@)jl-g@ciah%a@egq26do5k256hn!eegu)#i(qa-o"
-)
+SECRET_KEY = os.environ.get("SECRET_KEY", get_random_secret_key())
 
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 
 INSTALLED_APPS = [
