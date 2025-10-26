778277b8dc3eaf3f74effe4d0c17a58fc8233556
SadOns <sadonsgithub@yandex.ru>
2025-10-26 19:04:11 +0300

    add models

---
+++ backend/foodgram/food/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Tags(models.Model):
+    name = models.CharField(
+        max_length=25,
+        unique=True,
+    )
+    slug = models.SlugField(
+        unique=True,
+    )
+
+
+class Ingredients(models.Model):
+    name = models.CharField(
+        max_length=50,
+    )
+    unit = models.TextChoices()
+
+
+class Receipts(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор'
+    )
+    name = models.CharField(
+        max_length=150,
+        verbose_name='Название рецепта'
+    )
+    image = models.ImageField(
+        upload_to='media/receipts/'
+        verbose_name='Картинка рецепта'
+    )
+    description = models.TextField()
+    ingredients = models.ManyToManyField(
+        Ingredients
+    )
+    tag = models.ManyToManyField(
+        Tags
+    )
--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", get_random_secret_key())
 
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1").split(",")
 
 
 INSTALLED_APPS = [
